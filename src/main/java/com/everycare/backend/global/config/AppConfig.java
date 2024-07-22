be61cb8734cd81ef4dd7f4b70f4c722e166e7338
d0kyoung <ehrud0522@gmail.com>
2024-07-22 21:15:20 +0900

    backend dockerfile, docker-compose.yml 추가

--- src/main/java/com/everycare/backend/global/config/AppConfig.java
+++ src/main/java/com/everycare/backend/global/config/AppConfig.java
@@ -37,6 +37,7 @@ package com.everycare.backend.global.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.http.converter.FormHttpMessageConverter;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
@@ -58,6 +59,8 @@ public class AppConfig {
         List<HttpMessageConverter<?>> messageConverters = new ArrayList<>();
         messageConverters.add(new MappingJackson2HttpMessageConverter());
         messageConverters.add(new StringHttpMessageConverter(StandardCharsets.UTF_8));
+        messageConverters.add(new FormHttpMessageConverter());
+
 
         restTemplate.setMessageConverters(messageConverters);
 
