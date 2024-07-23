426c3ce08fc5c911a41771f8b66de21aa521fdb0
d0kyoung <ehrud0522@gmail.com>
2024-07-23 14:11:51 +0900

    spring security cors 수정

--- src/main/java/com/everycare/backend/global/config/WebConfig.java
+++ src/main/java/com/everycare/backend/global/config/WebConfig.java
@@ -9,7 +9,8 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/static/**")
-                .addResourceLocations("classpath:/static/");
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/")
+                .addResourceLocations("classpath:/templates/");
     }
 }
